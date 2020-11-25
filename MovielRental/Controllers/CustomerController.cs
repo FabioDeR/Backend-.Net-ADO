4cca11864a94753c895a648edb8aa13466304986
Psy Ravalez <Psychiatrietv02@gmail.com>
2020-11-25 09:49:00 +0100

    Custommer Controller ok

--- MovielRental/Controllers/CustomerController.cs
+++ MovielRental/Controllers/CustomerController.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using MovieDAL.Models;
 using MovieDAL.Services;
+using MovielRental.Infrastructure.Security;
+using MovielRental.Models.Forms;
+using MovielRental.Models.Service;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,33 +12,63 @@ using System.Threading.Tasks;
 
 namespace MovielRental.Controllers
 {
-    //[Route("api/[controller]")]
-    //[ApiController]
-    //public class CustomerController : ControllerBase
-    //{
-    //    private CustomerService _service;
-
-    //    public CustomerController()
-    //    {
-    //        _service = new CustomerService();
-    //    }
 
 
-    //    [HttpPost]
-        
-    //    [Route("/Login")]
-    //    public IActionResult GetCheck(Customer customer)
-    //    {
-    //        return Ok(_service.GetCheck(customer));
-    //    }
-
-    //    [HttpPost]
-    //    [Route("/Register")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly AuthService _authService;
+        private readonly TokenService _tokenService;
 
 
-    //    public IActionResult Insert(Customer entity)
-    //    {
-    //        return Ok(_service.Insert(entity));
-    //    }
-    //}
+
+        public CustomerController()
+        {
+            _authService = new AuthService();
+            _tokenService = new TokenService();
+        }
+
+        [HttpPost("Register")]
+        public IActionResult Register([FromBody] RegisterForm form)
+        {
+            if (ModelState.IsValid)
+            {
+                Customer customer = new Customer()
+                {
+                    LastName = form.LastName,
+                    FirstName = form.FirstName,
+                    Email = form.Email,
+                    Passwd = form.Passwd
+                };
+                _authService.Register(customer);
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPost("Login")]
+        public IActionResult Login([FromBody] LoginForm form)
+        {
+            if (ModelState.IsValid)
+            {
+                Customer customer = _authService.Login(form.Email, form.Passwd);
+
+                if (customer is null)
+                {
+                    return NotFound();
+                }
+
+                customer.Token = _tokenService.GenerateToken(customer);
+
+                return Ok(customer);
+            }
+
+            return BadRequest();
+        }
+
+
+    }
+
 }
